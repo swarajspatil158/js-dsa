eb8605a5d820008db8500ac280a7ae56c4c7728c
swarajspatil158 <swarajspatil158@gmail.com>
2022-05-26 23:22:30 +0530

    power of two

---
+++ powerOfTwo.js
@@ -0,0 +1,46 @@
+// Big-O = O(log(n))
+function powerOfTwo(num) {
+  if (num < 1) {
+    return false;
+  }
+  let result = 1;
+  while (result < num) {
+    result *= 2;
+  }
+  if (result > num) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Big-O = O(log(n))
+function powerOfTwoAnother(num) {
+  if (num < 1) {
+    return false;
+  }
+  while (num > 1) {
+    if (num % 2 !== 0) {
+      return false;
+    }
+    num = num / 2;
+  }
+  return true;
+}
+
+// Big-O = O(1)
+function powerOfTwoBitwise(num) {
+  if (num < 1) {
+    return false;
+  }
+  if ((num & (num - 1)) === 0) {
+    return true;
+  } else {
+    return false;
+  }
+  // OR return n & (n-1)
+}
+
+console.log(powerOfTwo(1));
+console.log(powerOfTwoAnother(2));
+console.log(powerOfTwoBitwise(5));
