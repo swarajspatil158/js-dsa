6d6ed1a5eb10aac308e7d56e21279d43ea2b38ac
swarajspatil158 <swarajspatil158@gmail.com>
2022-05-27 00:19:54 +0530

    recursive fibonacci

---
+++ fibonacciRecursive.js
@@ -0,0 +1,21 @@
+// Big-O = O(2^n)
+function fibonacciRecursive(num) {
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+  return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+}
+
+// Big-O = O(2^n)
+function fibonacciRecursiveAnother(num) {
+  if (num < 2) {
+    return num;
+  }
+  return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+}
+console.log(fibonacciRecursive(0));
+console.log(fibonacciRecursive(1));
+console.log(fibonacciRecursive(6));
