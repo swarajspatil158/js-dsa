321859aed6cd33ec7ac80c9d55637fa43f979d53
Swaraj Patil <swarajspatil158@gmail.com>
2022-07-02 12:05:47 +0530

    mergeSort

---
+++ algorithms/mergeSort.js
@@ -0,0 +1,29 @@
+// Big-O = O(nlogn)
+
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+  const sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+const arr = [7, 6, 5, 4, 3, 2, 1];
+const arr2 = [-7, 6, -5, 4, -3, 2, -1];
+
+console.log(mergeSort(arr));
+console.log(mergeSort(arr2));
